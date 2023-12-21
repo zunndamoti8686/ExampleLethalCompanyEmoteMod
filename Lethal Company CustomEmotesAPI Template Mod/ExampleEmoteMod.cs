b783fa61f66c0947d11690e07ecfe33505bff6db
Nunchuk/ToasterOven <49164897+ToastedOven@users.noreply.github.com>
2023-12-21 13:53:47 -0500

    project setup

--- Lethal Company CustomEmotesAPI Template Mod/ExampleEmoteMod.cs
+++ Lethal Company CustomEmotesAPI Template Mod/ExampleEmoteMod.cs
@@ -14,8 +14,8 @@ namespace Lethal_Company_CustomEmotesAPI_Template_Mod
     [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
     public class ExampleEmoteMod : BaseUnityPlugin
     {
-        public const string PluginGUID = "com.weliveinasociety.badasscompany";
-        public const string PluginName = "BadAssCompany";
+        public const string PluginGUID = "com.weliveinasociety.testmod";
+        public const string PluginName = "testmod";
         public const string PluginVersion = "1.0.0";
         public static PluginInfo PInfo { get; private set; }
         public static ExampleEmoteMod instance;
@@ -29,31 +29,35 @@ namespace Lethal_Company_CustomEmotesAPI_Template_Mod
 
 
             AnimationClipParams emoteParams = new AnimationClipParams();
-            emoteParams.animationClip = []; //list of primary animation clips, one of these will be picked randomly if you include more than one
-            emoteParams.secondaryAnimation = [];// list of secondary animation clips, must be the same size as animationClip, will be picked randomly in the same slot (so if we pick animationClip[3] for an animation, we will also pick secondaryAnimation[3])
+            emoteParams.animationClip = [CustomEmotesAPI_Template_Mod.Assets.Load<AnimationClip>("assets/punch.anim")]; //list of primary animation clips, one of these will be picked randomly if you include more than one
+            emoteParams.secondaryAnimation = null;// list of secondary animation clips, must be the same size as animationClip, will be picked randomly in the same slot (so if we pick animationClip[3] for an animation, we will also pick secondaryAnimation[3])
             emoteParams.looping = false;//used to specify if audio is looping, you only need to set this if you are importing a single audio file
-            emoteParams._primaryAudioClips = [];//primary list of audio clips
-            emoteParams._secondaryAudioClips = [];//secondary list of audio clips, if these are specified, the primary clip will never loop and the secondary clip that plays will always loop
-            emoteParams._primaryDMCAFreeAudioClips = [];//same as _primaryAudioClips but will be played if DMCA settings allow it (if normal audio clips exist, and dmca clips do not, the dmca clips will simply be silence)
-            emoteParams._secondaryDMCAFreeAudioClips = [];//same as _secondaryAudioClips but will be played if DMCA settings allow it
+            emoteParams._primaryAudioClips = null;//primary list of audio clips
+            emoteParams._secondaryAudioClips = null;//secondary list of audio clips, if these are specified, the primary clip will never loop and the secondary clip that plays will always loop
+            emoteParams._primaryDMCAFreeAudioClips = null;//same as _primaryAudioClips but will be played if DMCA settings allow it (if normal audio clips exist, and dmca clips do not, the dmca clips will simply be silence)
+            emoteParams._secondaryDMCAFreeAudioClips = null;//same as _secondaryAudioClips but will be played if DMCA settings allow it
             emoteParams.visible = true;// If false, will hide the emote from all normal areas, however it can still be invoked through PlayAnimation, use this for emotes that are only needed in code
-            emoteParams.syncAnim = true;// If true, will sync animation between all people emoting
-            emoteParams.syncAudio = true;// If true, will sync audio between all people emoting
+            emoteParams.syncAnim = false;// If true, will sync animation between all people emoting
+            emoteParams.syncAudio = false;// If true, will sync audio between all people emoting
             emoteParams.startPref = -1;// Spot in animationClip array where a BoneMapper will play when there is no other instance of said emote playing -1 is random, -2 is sequential, anything else is what you make it to be
             emoteParams.joinPref = -1;// Spot in animationClip array where a BoneMapper will play when there is at least one other instance of said emote playing, -1 is random, -2 is sequential, anything else is what you make it to be
-            emoteParams.joinSpots = [];// Array of join spots which will appear when the animation is playing
-            emoteParams.customName = "";// Custom name for emote, if not specified, the first emote from animationClip will be used as the name
+            emoteParams.joinSpots = null;// Array of join spots which will appear when the animation is playing
+            emoteParams.customName = "My Cool Punch";// Custom name for emote, if not specified, the first emote from animationClip will be used as the name
             emoteParams.lockType = AnimationClipParams.LockType.headBobbing;// determines the lock type of your emote, none, headBobbing, lockHead, or rootMotion
             emoteParams.willGetClaimedByDMCA = false;// Lets you mark if your normal set of audio will get claimed by DMCA
-            emoteParams.audioLevel = .7f;// determines the volume of the emote in terms of alerting enemies, 0 is nothing, 1 is max
+            emoteParams.audioLevel = 0;// determines the volume of the emote in terms of alerting enemies, 0 is nothing, 1 is max
 
+            CustomEmotesAPI.AddCustomAnimation(emoteParams);
 
             CustomEmotesAPI.animChanged += CustomEmotesAPI_animChanged;
         }
 
         private void CustomEmotesAPI_animChanged(string newAnimation, BoneMapper mapper)
         {
-            Debug.Log($"animation name: [{newAnimation}] is playing on player: [{mapper.basePlayerModelAnimator}]"); //I could also check mapper.mapperBody here to get the PlayerControllerB but this project doesn't include game files so I cannot.
+            if (newAnimation == "punch")
+            {
+                DebugClass.Log($"we punched");
+            }
         }
     }
 }
